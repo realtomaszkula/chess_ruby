b7aaf54299b8752fc499838107277952de8f97d0
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-29 14:00:50 +0100

    pawn promotion

--- chess.rb
+++ chess.rb
@@ -1,8 +1,33 @@
+require_relative = './pieces.rb'
+
 class Chess
 
   def initialize
 
-
   end
 
 end
+
+
+for i in 0..7
+  Pawn.new(:white, [1,i])
+  Pawn.new(:black, [6,i])
+end
+
+  Rook.new(   :white, [0,0])
+  Rook.new(   :white, [0,7])
+  Bishop.new( :white, [0,6])
+  Bishop.new( :white, [0,1])
+  Knight.new( :white, [0,5])
+  Knight.new( :white, [0,2])
+  Queen.new(  :white, [0,3])
+  King.new(   :white, [0,4])
+
+  Rook.new(   :black, [7,0])
+  Rook.new(   :black, [7,7])
+  King.new(   :black, [7,4])
+  Queen.new(  :black, [7,3])
+  Knight.new( :black, [7,2])
+  Knight.new( :black, [7,5])
+  Bishop.new( :black, [7,1])
+  Bishop.new( :black, [7,6])
--- pieces.rb
+++ pieces.rb
@@ -9,11 +9,65 @@ class Piece
   end
 
   def move
-    @position = movement_pattern
+    @position = possibe_moves.first
+    p @position
   end
 
 
 end
+
+class Pawn < Piece
+
+  attr_reader :color, :figure
+  attr_accessor :position
+
+
+  def initialize(color, position)
+    super(color, position)
+    @figure = :pawn
+    @moved = false
+    @promote_position = promote_position
+  end
+
+  def move
+    super
+    @moved = true
+    pawn_promotion if @promote_position.include?(@position)
+  end
+
+  def promote_position
+    x = @position[0]
+
+    case @color
+    when :white then return (0..7).collect {|i| [x+6, i]}
+    when :black then return (0..7).collect {|i| [x-6, i]}
+    end
+
+  end
+
+  def possibe_moves
+    x, y = @position[0], @position[1]
+    @moved ? [[x+1, y]] : [[x+1, y], [x+2, y]]
+  end
+
+
+  def pawn_promotion
+    puts "Time to promote! [QUEEN, KNIGHT, ROOK, BISHOP]"
+    case input = gets.chomp.upcase
+    when 'QUEEN'  then Queen.new(@color, @position)
+    when 'KNIGHT' then Knight.new(@color, @position)
+    when 'ROOK'   then Rook.new(@color, @position)
+    when 'BISHOP' then Bishop.new(@color, @position)
+    else puts 'Incorrect, try again'; pawn_promotion
+    end
+  end
+
+  def en_passant
+  end
+end
+
+
+
 
 class Knight < Piece
   attr_reader :color, :figure
@@ -70,70 +124,9 @@ class Rook < Piece
   end
 end
 
-class Pawn < Piece
-
-  attr_reader :color, :figure
-  attr_accessor :position
 
 
-  def initialize(color, position)
-    super(color, position)
-    @figure = :pawn
-    @moved = false
-    @promote_position = promote_position
-  end
 
-  def move
-    @moved = true
-    super
-  end
-
-  def promote_position
-    x = @position[0]
-
-    case @color
-    when :white then return (0..7).collect {|i| [x+6, i]}
-    when :black then return (0..7).collect {|i| [x-6, i]}
-    end
-
-  end
-
-  def movement_pattern
-    x, y = @position[0], @position[1]
-    moved ? [x+1, y] : [[x+1, y], [x+2, y]]
-
-  end
-
-
-  def pawn_promotion
-  end
-
-  def en_passant
-  end
-end
-
-for i in 0..7
-  Pawn.new(:white, [1,i])
-  Pawn.new(:black, [6,i])
-end
-
-  Rook.new(   :white, [0,0])
-  Rook.new(   :white, [0,7])
-  Bishop.new( :white, [0,6])
-  Bishop.new( :white, [0,1])
-  Knight.new( :white, [0,5])
-  Knight.new( :white, [0,2])
-  Queen.new(  :white, [0,3])
-  King.new(   :white, [0,4])
-
-  Rook.new(   :black, [7,0])
-  Rook.new(   :black, [7,7])
-  King.new(   :black, [7,4])
-  Queen.new(  :black, [7,3])
-  Knight.new( :black, [7,2])
-  Knight.new( :black, [7,5])
-  Bishop.new( :black, [7,1])
-  Bishop.new( :black, [7,6])
 
 
 

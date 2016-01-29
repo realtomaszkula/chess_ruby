db42b9b2a8ba917a8c9bc37ba74c4ead3d118efb
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-29 20:22:06 +0100

    selects selected figure

--- chess.rb
+++ chess.rb
@@ -1,14 +1,14 @@
 require_relative './pieces.rb'
 require_relative './player.rb'
 require_relative './board.rb'
-require 'colorize'
 
 class Chess
   attr_reader :plr1, :plr2, :board, :all_pieces
   def initialize
     make_players
     make_board
-    @all_pieces = plr1.pieces + plr2.pieces
+    @all_pieces = @plr1.pieces + @plr2.pieces
+    @active_player = @plr1
     # update_board
   end
 
@@ -19,30 +19,41 @@ class Chess
   def player_move
 
     ## mockup for rspec
-    # input = "70 34"
-    # input = input.split
+    input = "00 34"
+    input = input.split
 
-    input = input_move()
-    from = convert_to_integer(input[0])
-    to = convert_to_integer(input[1])
+    # input = input_move()
 
-    @selected_from = @board.value[from[0]][from[1]]
+    @selected_position = split_and_convert(input[0])
+    # @selected_unicode = @board.value[from[0]][from[1]] ## points to unicode representation
+    @selected_figure = @active_player.pieces.select { |piece| piece.position == @selected_position }
+
+    p @selected_figure
+    # unless @active_player.pieces.include?(@selected_figure)
+
+    to = split_and_convert(input[1])
     @selected_to = @board.value[to[0]][to[1]]
 
-    puts "from: #{@selected_from}, to: #{@selected_to}"
+    # puts "from: #{@selected_position}, to: #{@selected_to}"
 
   end
 
   def input_move
     puts "Enter your move [ to move from A1 to A2 type: A1 A2 ]"
     input = gets.chomp.downcase
-    until input.length == "5" && input[2] == " " && input[0].ord.between?(65,72) && input[3].ord.between?(65,72)  && input[1].to_i.between?(1,8)  && input[4].to_i.between?(1,8)
+    until input.length == "5"          &&
+          input[2]     == " "          &&
+          input[0].ord.between?(65,72) &&
+          input[3].ord.between?(65,72) &&
+          input[1].to_i.between?(1,8)  &&
+          input[4].to_i.between?(1,8)
        "Incorrect, try again"
+        input = gets.chomp.downcase
     end
     input = input.split ## ["70" "34"]
   end
 
-  def convert_to_integer(input)
+  def split_and_convert(input)
     input.split("").collect!(&:to_i)  ## [7,0]
   end
 
